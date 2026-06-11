f065cbdd867a89d8053ecb647733063b21e0d5d2
Vilkrin <contact@vilkrin.uk>
2026-06-11 17:38:42 +0100

    Removed Debugging Logging
    Removed a debugging logging line from the Github Webhook Controller

--- app/Http/Controllers/Webhooks/GitHubWebhookController.php
+++ app/Http/Controllers/Webhooks/GitHubWebhookController.php
@@ -21,8 +21,6 @@ class GitHubWebhookController extends Controller
 
         abort_unless(hash_equals($signature, $request->header('X-Hub-Signature-256', '')), 403);
 
-        \Log::info('GitHub webhook received', $request->all());
-
         if ($request->header('X-GitHub-Event') !== 'push') {
             return response()->json(['ignored' => true]);
         }
