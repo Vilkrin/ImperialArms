9f9d135d3c9436821146c6b895f250b8223b5274
Vilkrin <contact@vilkrin.uk>
2026-06-22 21:45:29 +0100

    wip
    wip

--- app/Models/UserProfile.php
+++ app/Models/UserProfile.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserProfile extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'tagline',
+        'bio',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
